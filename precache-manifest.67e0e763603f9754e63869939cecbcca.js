ad392137e13166ec54715ff20e642c6369440160
ufuk <ufuk.uysl5@gmail.com>
2020-08-07 12:21:30 +0200

    Updates

---
+++ precache-manifest.67e0e763603f9754e63869939cecbcca.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b24f8cbff1baf96f8cd25395190967c",
+    "url": "/covid-tracker-app/index.html"
+  },
+  {
+    "revision": "d4f35bc9bc40521ad402",
+    "url": "/covid-tracker-app/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "efa7bffe74997dedecfc",
+    "url": "/covid-tracker-app/static/css/main.a9d94b2f.chunk.css"
+  },
+  {
+    "revision": "d4f35bc9bc40521ad402",
+    "url": "/covid-tracker-app/static/js/2.3fa760cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-tracker-app/static/js/2.3fa760cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efa7bffe74997dedecfc",
+    "url": "/covid-tracker-app/static/js/main.3bc273d4.chunk.js"
+  },
+  {
+    "revision": "cb8fba4e24c6feab0a0a",
+    "url": "/covid-tracker-app/static/js/runtime-main.0c95cb21.js"
+  },
+  {
+    "revision": "02024ed46baaedeb9beb6aeab21fcc02",
+    "url": "/covid-tracker-app/static/media/affects.02024ed4.jpg"
+  },
+  {
+    "revision": "7d3ae8a316d22d25329313e89f8b72b8",
+    "url": "/covid-tracker-app/static/media/precautions.7d3ae8a3.jpg"
+  },
+  {
+    "revision": "d97d19abf0c80fafae9085fb729812f1",
+    "url": "/covid-tracker-app/static/media/world.d97d19ab.jpg"
+  }
+]);
